c2b2b5bf893eeedc643ffcbbc87e9244a52dc938
ansarSultan999 <ansarsultan2002@gmail.com>
2023-10-21 18:52:28 +0500

    chapter 1 to 4 assignment

---
+++ app.js
@@ -0,0 +1,167 @@
+                          //    Chapter no 1
+
+                //   Question no 1
+
+// alert("");
+
+                //   Question no 2
+
+// alert("Please enter a valid password.");
+
+               //    Question no 3
+               
+// alert("Welcome to JS LAND,,,\n Happy Coding!");
+
+               //    Question no 4
+
+// alert("Welcome to JS Land,,,");
+// alert("Happy Coding! \n pervent this code from creating additional dialogs."); sir box ki samajh nahi aai
+ 
+          
+               //    Question no 5
+
+
+// var name =("Hello i can run JS through my web browser's console");
+// console.log(name);
+
+                       //     Chapter no 2
+
+
+                    //    Question no 1
+
+
+// var userName ="";
+
+                    //   Question no 2
+                    
+// var myName = alert("Ansar Sultan");
+
+                       //   Question no 3
+
+// var message = alert("Hello World");
+
+                       //  Question no 4
+
+// var name = alert("Ansar Sultan");
+// var age = alert("17 years old \n pervent this page from creating additional dialogs.");
+// var tag = alert("SSUET");
+// var bio = alert("Mobile & Cloud Computing - Module A \n prevent this page from creating additional dialogs")
+
+                       //  Question no 5
+                       
+// var  food = alert("PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+                     //     Question no 6 
+
+
+// var email="ansarsultan2002@gmail.com";
+// alert("My email adress is:"+email);
+
+                       //      Question no 7
+
+
+// var book="A smarter way to learn JavaScript";
+// alert("i am trying to learn from the book' "+ book);
+
+                         //     Question no  8
+
+//    var studentName = "Ansar Sultan";
+
+// var age = 21;
+
+// var course = "SMIT";
+
+// var country = "Pakistan";
+
+// alert boxes
+
+
+// alert("Student Name: " + studentName);
+
+// alert("Age: " + age);
+
+// alert("Course: " + course);
+
+// alert("Country: " + country);
+
+                          //    Question no 9
+
+
+// var text = "Yah! I can write HTML content through JavaScript ";
+//         document.write(text);
+
+                         // Question no 10
+
+
+// var partner = " My partner name is mahnoor";
+// var child = "I have two childrens";
+// var loc = "I live in pakistan";
+// var job = "I am Web and Mobile app Develper"
+
+// alert(partner);
+// alert(child);
+// alert(loc);
+// alert(job);
+
+
+                       // CHAPTER NO 3
+
+
+
+                          
+// Q.NO-1
+// Ans:
+// var age="21"
+// alert("I am " + age+ " years old");
+
+// Q.NO-2
+// Ans:
+// -------------------------------------------------------------------------------------------------
+
+
+// Q.No-3
+// Ans:
+// var birthYear = 2002;
+// alert("My birth year is " + birthYear);
+// document.write("My birth Year is " + birthYear);
+
+
+// Q.No-4
+// Ans:
+// var visitorName = prompt("please enter your name:");
+// var productTitle = prompt("Please enter the product title you want to order:")
+// var quantity = prompt("Please enter the quantity you want to order:")
+// var message = visitorName + " ordered " + quantity + " " + productTitle + "(s) on Sultan Clothing store";
+// document.write(message);
+
+
+
+                           // CHAPTER NO 4
+
+
+                        // Question no 1
+
+
+// var  name = "Ali Khan"
+// var  age = 17
+// var  school = "SSUET"
+// var stat = "My name is " + name + " my age is "+age +" my school name is " +school;
+// document.write(stat);
+
+                         //Question no 2
+//  myName
+// age1
+// _score
+// camelCaseVariable
+// a_variable_with_underscore
+
+// Illegal Variable Names:
+
+// 1name (Variable names cannot start with a number)
+// my-name (Variable names cannot contain hyphens)
+// @symbol (Variable names cannot contain special symbols)
+// class (Reserved keywords, like "class," cannot be used as variable names)
+// my variable (Variable names cannot contain spaces)
+
+                           // COMPLETE FIRST ASSIGNMENT
+
